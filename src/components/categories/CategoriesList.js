debb8ab815e692075686b96e2aa9ac44e48828ce
Gregory Haney <gregory.haney@gmail.com>
2022-04-22 11:23:36 -0500

    CatagoriesForm

--- src/components/categories/CategoriesList.js
+++ src/components/categories/CategoriesList.js
@@ -6,10 +6,14 @@
 */
 
 import React, { useEffect, useState } from "react";
-import { getCategories } from "./CategoriesManager";
+import { useHistory } from "react-router-dom";
+import { getCategories, deleteCategory } from "./CategoriesManager";
+import { editCategory, addCategory } from "./CategoriesManager";
 
 export const CategoriesList = () => {
     const [ categories, setCategories ] = useState([])
+    const [ category, setCategory ] = useState({})
+    const history = useHistory()
 
     useEffect(() => {
         getCategories()
@@ -17,27 +21,77 @@ export const CategoriesList = () => {
     }, [])
 
 
+    const saveCategory = () => {
+        const newCategory = {
+            label: category.label
+        }
+
+        addCategory(newCategory)
+            .then(() => {
+                setCategory({label: ""})
+                getCategories()
+                    .then((category) => setCategories(category))
+            })
+    }
+
     return (
-        <>
-        {
+        <div className="categorieslist">
+            <div className="allcategories">
+            {
             categories.map(
                 (category) => {
-                    return <div className="category" key={`category--${category.id}`}>
+                    return (
+                    <>
+                        <div className="category" key={`category--${category.id}`}>
                             <article className="categorylist">                           
                                 Category: {category.label}  
 
                                 <button className="button" onClick={() => {
-                                history.push(`EditRequest/${request.id}`)
-                            }}>Edit Request</button> 
+                                history.push(`editCategory/${category.id}`)
+                                 }}>Edit Category</button> 
 
                                 <button className="button" onClick={() => {
-                                    deleteRequest(request.id)                            
-                                }}>Delete Request</button>                         
+                                    deleteCategory(category.id)                            
+                                }}>Delete Category</button>                         
                             </article>
                         </div>
+                    </>
+                    )
                 }
-            )
-        }
-        </>
+            )}
+            </div>
+                    <div className="addcategory">
+                        <form className="newCategoryForm">
+                        <h2 className="newCategoryForm__title">Create a New Category</h2>
+
+                        <fieldset>
+                            <div className="form-group">
+                                <label htmlFor="label">New category:</label>
+                                <input
+                                    onChange={
+                                        (evt) => {
+                                            const copy = {...category}
+                                            copy.label = evt.target.value
+                                            setCategory(copy)
+                                        }
+                                    }
+                                    required autoFocus
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Enter the new category..."
+                                    />
+                            </div>
+                        </fieldset>  
+                                <br></br>
+                                <button type="submit" className="btn btn-primary"
+                                    onClick={(evt) => {
+                                        evt.preventDefault()
+                                        saveCategory()
+                                    }}
+                                >Submit
+                                </button>
+                        </form>
+                    </div>
+        </div>
     )
 }
